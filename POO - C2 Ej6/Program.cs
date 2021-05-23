9483a734ef592036bccc87956bdceca429cc80c7
Agneta <agnetagsu@yahoo.es>
2021-05-23 19:37:35 +0200

    Agregar archivos de proyecto.

---
+++ POO - C2 Ej6/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace POO___C2_Ej6
+{
+    class Coche
+    {
+        private string marca;
+        private string modelo;
+        private int cilindrada;
+        private decimal potencia;
+
+
+        public String Marca
+        {
+            get
+            {
+                return marca;
+            }
+            set
+            {
+                marca = value;
+            }
+        }
+        public String Modelo
+        {
+            get
+            {
+                return modelo;
+            }
+            set
+            {
+                modelo = value;
+            }
+        }
+        public int Cilindrada
+        {
+            get
+            {
+                return cilindrada;
+            }
+            set
+            {
+                cilindrada = value;
+            }
+        }
+        public decimal Potencia
+        {
+            get
+            {
+                return potencia;
+            }
+            set
+            {
+                potencia = value;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Coche micoche = new Coche();
+
+            micoche.marca = "Toyota";
+            micoche.modelo = "Avensis D4D";
+            micoche.cilindrada = 4;
+            micoche.potencia = 3600;
+
+            Console.WriteLine("Marca: {0} Modelo: {1} con {2} cilindros y {3} de potencia", micoche.marca, micoche.modelo,micoche.cilindrada,micoche.potencia);
+        }
+    }
+}
